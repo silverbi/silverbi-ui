086fa237791184fbf0d39791f78fd1df5fdb1ce2
현은비 <silverbi@bumbis-mac.local>
2024-01-21 02:50:06 +0900

    fix: remove storybook sample files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+import svgr from 'vite-plugin-svgr'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
